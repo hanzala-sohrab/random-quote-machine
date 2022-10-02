3308c5d979ee36de042df7a00c626c2c7f194ee6
Hanzala Sohrab <sorb.hanzala@gmail.com>
2022-10-02 17:21:05 +0530

    Setup GitHub Pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   };
   return (
     <div className="App">
-      <div className="App-header" id="quote-box">
+      <div id="quote-box">
         <h4 id="text">{quote.text}</h4>
         <p id="author">- {quote.author}</p>
         <button id="new-quote">New quote</button>
